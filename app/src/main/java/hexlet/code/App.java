9a2570773d4e72d3d9f557fbf8526bffed7bb526
maksim butakov <qulifis@gmail.com>
2023-02-26 17:19:29 +0600

    Add functionality to choose game number

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,32 @@
 package hexlet.code;
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.getUserName();
+        Scanner scanner = new Scanner(System.in);
+        String gameNumber;
+
+        System.out.print("Please enter the game number and press Enter.\n"
+                + "1 - Greet\n"
+                + "2 - Even\n"
+                + "0 - Exit\n"
+                + "Your choice: ");
+        gameNumber = scanner.next();
+
+        while (true) {
+            if (gameNumber.equals("0")) {
+                break;
+            } else if (gameNumber.equals("1")) {
+                Cli.greetUser();
+                break;
+            } else if (gameNumber.equals("2")) {
+                Even.playGame();
+                break;
+            } else {
+                System.out.print("Please enter a valid number: ");
+                gameNumber = scanner.next();
+            }
+        }
     }
 }
