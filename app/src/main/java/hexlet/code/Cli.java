99faaddf51b2f3add8aa33daed4f3282bce39e87
maksim butakov <qulifis@gmail.com>
2023-02-27 14:21:39 +0600

    Add userName staic parameter

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,14 +3,13 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static String greetUser() {
+    public static String userName;
+    public static void greetUser() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
-        String userName = scanner.next();
+        userName = scanner.next();
         System.out.printf("Hello, %s!\n", userName);
-
-        return userName;
     }
 }
